446fdd2cee4b5331e2404ee19c0c1543e8c76144
srb3 <sbrown@chef.io>
2020-07-06 10:31:30 +0100

    switching to ListenerPortsAndProtocol, removing some debug puts

--- libraries/alicloud_slb.rb
+++ libraries/alicloud_slb.rb
@@ -20,10 +20,8 @@ class AliCloudSlb < AliCloudResourceBase
     opts = { slb_id: opts } if opts.is_a?(String)
     opts[:slb_id] = opts.delete(:id) if opts.key?(:id)
 
-    puts "options #{opts.inspect}"
     super(opts)
     validate_parameters(required: %i(slb_id))
-    puts "opts[:slb_id] #{opts[:slb_id]} and opts[:region] #{opts[:region]}" if opts[:slb_id] && opts[:region]
     catch_alicloud_errors do
       @resp = @alicloud.slb_client.request(
         action: 'DescribeLoadBalancerAttribute',
@@ -45,7 +43,7 @@ class AliCloudSlb < AliCloudResourceBase
     @status                      = @slb_info['LoadBalancerStatus']
     @resource_group_id           = @slb_info['ResourceGroupId']
     @address                     = @slb_info['Address']
-    @listener_ports_and_protocol = @slb_info['ListenerPortsAndProtocal']['ListenerPortAndProtocal']
+    @listener_ports_and_protocol = @slb_info['ListenerPortsAndProtocol']['ListenerPortAndProtocol']
     @backend_servers             = @slb_info['BackendServers']
     @has_reserved_info           = @slb_info['HasReservedInfo']
     @business_status             = @slb_info['BusinessStatus']
@@ -75,19 +73,19 @@ class AliCloudSlb < AliCloudResourceBase
   end
 
   def https_listeners?
-    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocal'] == 'https' }.length > 0
+    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocol'] == 'https' }.length > 0
   end
 
   def http_listeners?
-    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocal'] == 'http' }.length > 0
+    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocol'] == 'http' }.length > 0
   end
 
   def udp_listeners?
-    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocal'] == 'udp' }.length > 0
+    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocol'] == 'udp' }.length > 0
   end
 
   def tcp_listeners?
-    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocal'] == 'tcp' }.length > 0
+    @listener_ports_and_protocol.select { |lpp| lpp['ListenerProtocol'] == 'tcp' }.length > 0
   end
 
   def listening_ports
@@ -95,19 +93,19 @@ class AliCloudSlb < AliCloudResourceBase
   end
 
   def https_ports
-    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocal'] == 'https'}.compact
+    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocol'] == 'https'}.compact
   end
 
   def http_ports
-    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocal'] == 'http'}.compact
+    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocol'] == 'http'}.compact
   end
 
   def udp_ports
-    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocal'] == 'udp'}.compact
+    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocol'] == 'udp'}.compact
   end
 
   def tcp_ports
-    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocal'] == 'tcp'}.compact
+    @listener_ports_and_protocol.map { |lpp| lpp['ListenerPort'] if lpp['ListenerProtocol'] == 'tcp'}.compact
   end
 
   def https_only?
@@ -118,6 +116,6 @@ class AliCloudSlb < AliCloudResourceBase
     slb = ''
     slb += "ID: #{@load_balancer_id} " if @load_balancer_id
     slb += "Name: #{@load_balancer_name} " if @load_balancer_name
-    opts.key?(:alicloud_region) ? "Server Load Balaner: #{slb} in #{opts[:alicloud_region]}" : "Server Load Balancer: #{slb}"
+    opts.key?(:region) ? "Server Load Balacner: #{slb} in #{opts[:region]}" : "Server Load Balancer: #{slb}"
   end
 end
