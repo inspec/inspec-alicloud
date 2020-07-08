c5ee4954e1418b9d6881b1e83bd07ab797e9315b
Gavin Reynolds <gavin@chef.io>
2020-07-08 13:33:15 +0100

    Fix control title
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/integration/verify/controls/alicloud_sts_caller_identity.rb
+++ test/integration/verify/controls/alicloud_sts_caller_identity.rb
@@ -2,7 +2,7 @@ title 'Ensure AliCloud credentials being used for the Inspec scan have the corre
 
 control 'alicloud-sts-caller-identity-1.0' do
   impact 1.0
-  title 'Make sure we are not on GovCloud'
+  title 'Ensure AliCloud STS caller identity has the correct properties.'
 
   describe alicloud_sts_caller_identity do
     it { should exist }
