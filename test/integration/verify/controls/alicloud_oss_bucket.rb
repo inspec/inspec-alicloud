9f222a929fab4ca0a182710e7c431627c3268ab7
Gavin Reynolds <gavin@chef.io>
2020-07-08 16:46:56 +0100

    Add OSS bucket tests
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- test/integration/verify/controls/alicloud_oss_bucket.rb
+++ test/integration/verify/controls/alicloud_oss_bucket.rb
@@ -1,6 +1,14 @@
 title 'Test single AliCloud OSS Bucket'
 
 alicloud_action_trail_bucket_id = input(:alicloud_action_trail_bucket_id, value: '', description: 'Action trail bucket name')
+alicloud_bucket_acl_name = input(:alicloud_bucket_acl_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_encrypted_name = input(:alicloud_bucket_encrypted_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_lifecycle_name = input(:alicloud_bucket_lifecycle_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_logging_name = input(:alicloud_bucket_logging_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_logging_target_name = input(:alicloud_bucket_logging_target_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_tags_name = input(:alicloud_bucket_tags_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_versioning_name = input(:alicloud_bucket_versioning_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_website_name = input(:alicloud_bucket_website_name, value: '', description: 'OSS bucket name')
 
 control 'alicloud-ossbucket-1.0' do
   impact 1.0
@@ -13,6 +21,48 @@ control 'alicloud-ossbucket-1.0' do
   describe alicloud_oss_bucket(bucket_name: alicloud_action_trail_bucket_id) do
     it { should exist }
     it { should_not be_public }
-    its('has_default_encryption_enabled') { should eq true }
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_acl_name) do
+    it { should exist }
+    it { should_not be_public }
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_encrypted_name) do
+    it { should exist }
+    it { should have_default_encryption_enabled }
+    its('bucket_lifecycle_rules') { should be_empty }
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_lifecycle_name) do
+    it { should exist }
+    its('bucket_lifecycle_rules') { should_not be_empty }
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_logging_name) do
+    it { should exist }
+    it { should have_access_logging_enabled }
+  end
+
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_logging_target_name) do
+    it { should exist }
+    it { should_not have_access_logging_enabled }
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_tags_name) do
+    it { should exist }
+    # its('tags')   { should include('Environment' => 'Dev',
+    #   'Name' => aws_bucket_public_name)}
+  end
+  
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_versioning_name) do
+    it { should exist }
+    it { should have_versioning_enabled }
+  end
+
+  describe alicloud_oss_bucket(bucket_name: alicloud_bucket_website_name) do
+    it { should exist }
+    it { should_not have_versioning_enabled }
+    it { should have_website_enabled }
   end
 end
--- test/integration/verify/controls/alicloud_oss_buckets.rb
+++ test/integration/verify/controls/alicloud_oss_buckets.rb
@@ -1,7 +1,14 @@
 title 'Test AliCloud OSS Buckets in bulk'
 
-alicloud_oss_bucket_public_name = attribute(:alicloud_oss_bucket_public_name, default: '', description: 'The AliCloud OSS bucket name.')
-alicloud_oss_bucket_private_name = attribute(:alicloud_oss_bucket_private_name, default: '', description: 'The AliCloud OSS bucket name.')
+alicloud_action_trail_bucket_id = input(:alicloud_action_trail_bucket_id, value: '', description: 'Action trail bucket name')
+alicloud_bucket_acl_name = input(:alicloud_bucket_acl_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_encrypted_name = input(:alicloud_bucket_encrypted_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_lifecycle_name = input(:alicloud_bucket_lifecycle_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_logging_name = input(:alicloud_bucket_logging_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_logging_target_name = input(:alicloud_bucket_logging_target_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_tags_name = input(:alicloud_bucket_tags_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_versioning_name = input(:alicloud_bucket_versioning_name, value: '', description: 'OSS bucket name')
+alicloud_bucket_website_name = input(:alicloud_bucket_website_name, value: '', description: 'OSS bucket name')
 
 control 'alicloud-oss-buckets-1.0' do
 
@@ -10,9 +17,16 @@ control 'alicloud-oss-buckets-1.0' do
 
   describe alicloud_oss_buckets do
     it { should exist }
-    its('count') { should be >= 1 }
-    its('bucket_names') { should include alicloud_oss_bucket_public_name }
-    its('bucket_names') { should include alicloud_oss_bucket_private_name }
+    its('count') { should be >= 9 }
+    its('bucket_names') { should include alicloud_action_trail_bucket_id }
+    its('bucket_names') { should include alicloud_bucket_acl_name }
+    its('bucket_names') { should include alicloud_bucket_encrypted_name }
+    its('bucket_names') { should include alicloud_bucket_lifecycle_name }
+    its('bucket_names') { should include alicloud_bucket_logging_name }
+    its('bucket_names') { should include alicloud_bucket_logging_target_name }
+    its('bucket_names') { should include alicloud_bucket_tags_name }
+    its('bucket_names') { should include alicloud_bucket_versioning_name }
+    its('bucket_names') { should include alicloud_bucket_website_name }
     its('bucket_names') { should_not include 'not-there-hopefully' }
   end
 end
