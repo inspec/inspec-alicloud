836fcabcc4d1ebe6ecd8dfb87f660815a56c5705
Jason McAdam <jason.mcadam@sap.com>
2020-08-12 12:25:41 +0100

    libraries: access keys:: refactor test
    Signed-off-by: Jason McAdam <jason.mcadam@sap.com>

--- test/integration/verify/controls/alicloud_ram_access_keys.rb
+++ test/integration/verify/controls/alicloud_ram_access_keys.rb
@@ -5,9 +5,7 @@ control 'alicloud-access-keys-1.0' do
     title 'Ensure Alicloud access key library has correct properties'
 
     describe alicloud_access_keys do
-        its('entries.count')  { should eq 1 }
-        its('entries.first.access_key_id') { should eq ENV['ALICLOUD_ACCESS_KEY'] }  # gets key of running user
-        its('entries.first.status') { should eq "Active" }
-        its('entries.first.create_date') { should_not be_nil }
+        it { should exist }
+        its('access_key_ids') { should include ENV['ALICLOUD_ACCESS_KEY'] }  # gets key of running user
     end
 end
