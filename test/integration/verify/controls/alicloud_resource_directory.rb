f71b2f14b74854e962077b9625ceae94d2dae142
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-07 12:47:42 +0530

    adding the missing test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/integration/verify/controls/alicloud_resource_directory.rb
@@ -0,0 +1,12 @@
+title 'Test single AliCloud Resource Directory'
+
+control 'alicloud_resource_directory-1.0' do
+  impact 1.0
+  title 'Ensure AliCloud Resource Directory has the correct properties.'
+
+  describe alicloud_resource_directory do
+    it { should exist}
+    its( 'resource_directory_id' ) {should_not eq 'rd_id'}
+    its( 'master_account_name' ) {should_not eq 'master_acct_name'}
+  end
+end
---
+++ test/integration/verify/controls/alicloud_slb_https_listener.rb
@@ -0,0 +1,18 @@
+alicloud_slb_https_id = input(:alicloud_slb_https_id, value: '', description: 'AliCloud slb https ID.')
+
+title 'Test single AliCloud Server Load Balance HTTPS Listener'
+
+control 'alicloud_slb_https_listener-1.0' do
+  impact 1.0
+  title 'Ensure AliCloud Server Load Balancer has the correct properties.'
+
+  describe alicloud_slb_https_listener(slb_id: alicloud_slb_https_id, listener_port: 443) do
+    it { should exist }
+  end
+  
+  alicloud_slb(alicloud_slb_https_id).https_ports.each do |port|
+    describe alicloud_slb_https_listener(slb_id: alicloud_slb_https_id, listener_port: port) do
+      its('tls_cipher_policy') { should eq 'tls_cipher_policy_1_2' }
+    end
+  end
+end
