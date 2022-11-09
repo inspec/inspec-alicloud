59d27b469a16e3c7605bd1db631783f2a0596954
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-09 11:43:18 +0530

    fixing lint errors
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

--- test/integration/verify/controls/alicloud_ims_sso.rb
+++ test/integration/verify/controls/alicloud_ims_sso.rb
@@ -3,7 +3,7 @@ title 'Test AliCloud IMS SSO Properties'
 control 'alicloud_ims_sso-1.0' do
   impact 1.0
   title 'Ensure AliCloud IMS SSO has correct attributes.'
-  
+
   describe alicloud_ims_sso do
     it { should exist }
     it { have_sso_enabled }
--- test/integration/verify/controls/alicloud_slb_https_listener.rb
+++ test/integration/verify/controls/alicloud_slb_https_listener.rb
@@ -9,7 +9,7 @@ control 'alicloud_slb_https_listener-1.0' do
   describe alicloud_slb_https_listener(slb_id: alicloud_slb_https_id, listener_port: 443) do
     it { should exist }
   end
-  
+
   alicloud_slb(alicloud_slb_https_id).https_ports.each do |port|
     describe alicloud_slb_https_listener(slb_id: alicloud_slb_https_id, listener_port: port) do
       its('tls_cipher_policy') { should eq 'tls_cipher_policy_1_2' }
