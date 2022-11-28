8c9562da3d3bc00cd2a82532e193f09b480653e5
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-28 12:13:02 +0530

    adding the action_trial unit tests
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/unit/resources/alicloud_actiontrail_trail_test.rb
@@ -0,0 +1,26 @@
+require 'helper'
+require 'alicloud_actiontrail_trail'
+
+class AliCloudActionTrailTrailConstructorTest < Minitest::Test
+  def setup
+    ENV['ALICLOUD_REGION'] = 'us-east-1'
+  end
+    
+  def test_empty_params_not_ok
+    assert_raises(ArgumentError) { AliCloudActionTrailTrail.new }
+  end
+  
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AliCloudActionTrailTrail.new(rubbish: 9) }
+  end
+  
+  def test_accepts_string_argument
+    at = AliCloudActionTrailTrail.new('not-there')
+    assert_equal 'not-there', at.trail_name
+  end
+  
+  def test_accepts_key_value_argument_and_resource_works
+    at = AliCloudActionTrailTrail.new(trail_name: 'test-trial')
+    assert_equal 'test-trial', at.trail_name
+  end
+end
---
+++ test/unit/resources/alicloud_actiontrail_trails_test.rb
@@ -0,0 +1,22 @@
+require 'helper'
+require 'alicloud_actiontrail_trails'
+
+class AliCloudActionTrailTrailsConstructorTest < Minitest::Test
+  def setup
+    ENV['ALICLOUD_REGION'] = 'us-east-1'
+    
+    AliCloudActionTrailTrails.any_instance.stubs(:fetch_data).returns({ 'Name' => 'trail-name',
+                                                                        'OssBucketName' => 'bucket-name',
+                                                                        'OssKeyPrefix' => 'bucket-name-prefix',
+                                                                        'RoleName' => 'role-name'
+                                                                      })
+  end
+  
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AliCloudActionTrailTrails.new(rubbish: 9) }
+  end
+  
+  def test_rejects_string_argument
+    assert_raises(ArgumentError) { AliCloudActionTrailTrails.new('not-there') }
+  end
+end
