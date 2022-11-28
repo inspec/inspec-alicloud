0cc8741f8fb602c4355d07b0a1db8a10288f91ea
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-28 11:52:19 +0530

    adding the rd test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/unit/resources/alicloud_rd_test.rb
@@ -0,0 +1,30 @@
+require 'helper'
+require 'alicloud_rd'
+
+class AliCloudResourceDirectoryConstructorTest < Minitest::Test
+  def setup
+    AliCloudResourceDirectory.any_instance.stubs(:fetch_db_info).returns({ 'ResourceDirectory' =>
+                                                                             {
+                                                                               'RootFolderId' => 'test-root_folder_id',
+                                                                               'ResourceDirectoryId' => 'test-id',
+                                                                               'CreateTime' => '2019-02-18T15:32:10.473Z',
+                                                                               'MasterAccountId' => 'test-master_account_id',
+                                                                               'MasterAccountName' => 'aliyun-admin',
+                                                                               'ControlPolicyStatus' => 'Enabled',
+                                                                               'MemberDeletionStatus' => 'Enabled',
+                                                                               'IdentityInformation' => 'test_identity_info'
+                                                                             }
+                                                                         })
+  end
+  
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AliCloudResourceDirectory.new(rubbish: 9) }
+  end
+  
+  def test_accepts_key_value_argument_and_resource_works
+    rd = AliCloudResourceDirectory.new()
+    assert_equal nil, rd.resource_directory_id
+    assert_equal nil, rd.master_account_name
+    assert_equal nil, rd.master_account_id
+  end
+end
