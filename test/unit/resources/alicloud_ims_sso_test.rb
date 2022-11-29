338fe79f0199513d9d232934dece399a2e0c8bdb
Soumyodeep Karmakar <soumyo.k13@gmail.com>
2022-11-29 14:45:03 +0530

    adding the ims sso unit test
    Signed-off-by: Soumyodeep Karmakar <soumyo.k13@gmail.com>

---
+++ test/unit/resources/alicloud_ims_sso_test.rb
@@ -0,0 +1,18 @@
+require 'helper'
+require 'alicloud_ims_sso'
+
+class AliCloudSsoSettingsConstructorTest < Minitest::Test
+  def setup
+    ENV['ALICLOUD_REGION'] = 'us-east-1'
+  end
+  
+  def test_rejects_unrecognized_params
+    assert_raises(ArgumentError) { AliCloudSsoSettings.new(rubbish: 9) }
+  end
+  
+  def test_accepts_key_value_argument_and_resource_works
+    ss = AliCloudSsoSettings.new()
+    assert_equal false, ss.sso_enabled
+    assert_equal nil, ss.auxiliary_domain
+  end
+end
